8037f9dd0630238a3d3f8f4135ebe7899e146317
Ara <ayaghsizian@intracitygeeks.org>
2017-07-20 14:59:21 -0400

    commented out files

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+
+
+
+// var mongoose = require("mongoose");
+// var dburl = "mongodb://localhost:27017/businesses";
+//
+// mongoose.connect(dburl);
+//
+// mongoose.connection.on('connected', function(){
+//   console.log("mongoose is connected to : " + dburl);
+// });
+//
+// mongoose.connection.on('disconnected', function(){
+//   console.log("mongoose is disconnected to : " + dburl);
+// });
+//
+// mongoose.connection.on('error', function(err){
+//   console.log("mongoose is errorconnected to : " + err);
+// });
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+
+// require("./db/connection.js").open();
+// var express = require('express');
+// var app = express();
+// var path = require('path');
+// var bodyParser = require('body-parser');
+// var routes = require('./api/routes');
+//
+// app.set('port', 3000);
+// app.use(function(req, res, next){
+//   console.log(req.method, req.url);
+// });
+//
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use(bodyParser.urlencoded({ extended: flase}));
+//
+// app.use('/api', routes);
+//
+// var server = app.listen(app.get('port'), function(){
+//   var port = server.address().port;
+//   console.log("Ara, we are on port: " + port);
+// });
